05255a21317af41a4c26edf3e659d5813ad3fe2a
Andrii Hrytsai <andriyhrytsay@gmail.com>
2021-04-14 13:45:36 +0300

    hm_7 не повна

--- hm_7/main.js
+++ hm_7/main.js
@@ -121,7 +121,45 @@
 // table(8,5,'a');
 
 // - Напишите «Карусель» – ленту изображений, которую можно листать влево-вправо нажатием на стрелочки.
-// не знаю як робити
+// const arrCar = [
+//     {
+//         alt: 'carOne',
+//         url: './img/gelik.jpg'
+//     },
+//     {
+//         alt: 'carTwo',
+//         url: './img/bmw.jpg'
+//     },
+//     {
+//         alt: 'carThree',
+//         url: './img/audi.jpg'
+//     },
+// ]
+// let leftButton = document.getElementById('leftClick');
+// let rightButton = document.getElementById('rightClick');
+// const imgContainer = document.getElementById('imgContainer')
+// let wrapper = 0;
+// imgContainer.style.width = '500px';
+// leftButton.onclick = () => {
+//     if (wrapper - 1 < 0) {
+//         wrapper = arrCar.length - 1;
+//     } else {
+//         wrapper = wrapper - 1;
+//     }
+//     imgContainer.src = arrCar[wrapper].url;
+//     imgContainer.alt = arrCar[wrapper].alt
+// }
+// rightButton.onclick = () => {
+//     if (wrapper + 1 > arrCar.length - 1) {
+//         wrapper = 0;
+//     } else {
+//         wrapper = wrapper + 1;
+//     }
+//     imgContainer.src = arrCar[wrapper].url;
+//     imgContainer.alt = arrCar[wrapper].alt;
+//     imgContainer.setAttribute('src', arrCar[wrapper].url);
+// }
+
 
 //- Сворити масив не цензцрних слів.
 // Сворити інпут текстового типу.
@@ -171,46 +209,46 @@
 // 2й - оставляет старше 29 лет включительно
 // 3й - оставляет тех у кого город киев
 // Данные выводить в документ
-const usersWithAddress = [
-    {id: 9, name: 'vasya', age: 31, isMarried: false, address: {city: 'Kyiv', street: 'Gongadze', number: 16}},
-    {id: 2, name: 'petya', age: 30, isMarried: true, address: {city: 'Rivne', street: 'Zelena', number: 1}},
-    {id: 4, name: 'kolya', age: 29, isMarried: true, address: {city: 'Lviv', street: 'Pasichna', number: 121}},
-    {id: 3, name: 'olya', age: 28, isMarried: false, address: {city: 'Rivne', street: 'Shevchenko', number: 90}},
-    {id: 8, name: 'max', age: 30, isMarried: true, address: {city: 'Lviv', street: 'Kriva Lipa', number: 115}},
-    {id: 6, name: 'anya', age: 31, isMarried: false, address: {city: 'Lviv', street: 'Shevchenko', number: 2}},
-    {id: 10, name: 'oleg', age: 28, isMarried: false, address: {city: 'Kyiv', street: 'Centralna', number: 22}},
-    {id: 5, name: 'andrey', age: 29, isMarried: true, address: {city: 'Lviv', street: 'Gorodotska', number: 43}},
-    {id: 1, name: 'masha', age: 30, isMarried: true, address: {city: 'Kyiv', street: 'Peremogi', number: 12}},
-    {id: 7, name: 'olya', age: 31, isMarried: false, address: {city: 'Lviv', street: 'Naukova', number: 16}},
-    {id: 11, name: 'max', age: 31, isMarried: true, address: {city: 'Rivne', street: 'Ivana Franka', number: 121}}
-];
-
-const main = document.getElementById('main');
-const render = (userArr, target) => {
-    target.innerHTML = '';
-    userArr.forEach(value => {
-        const div = document.createElement('div');
-        div.innerText = JSON.stringify(value);
-        target.appendChild(div)
-    })
-}
-render(usersWithAddress, main)
-
-const form = document.forms.myForm;
-form.onchange = () => {
-    const {status, gt, city} = form
-    let res = usersWithAddress;
-    // 1й - отфильтровывает пользователей со статусом false (осталяет со статусом false)
-    if (status.checked) {
-        res = res.filter(value => !value.status);
-    }
-    // 2й - оставляет старше 29 лет включительно
-    if (gt.checked) {
-        res = res.filter(value => value.age >= 29)
-    }
-    // 3й - оставляет тех у кого город киев
-    if (city.checked) {
-        res = res.filter(value => value.address.city === 'Kyiv')
-    }
-    render(res, main);
-}
+// const usersWithAddress = [
+//     {id: 9, name: 'vasya', age: 31, isMarried: false, address: {city: 'Kyiv', street: 'Gongadze', number: 16}},
+//     {id: 2, name: 'petya', age: 30, isMarried: true, address: {city: 'Rivne', street: 'Zelena', number: 1}},
+//     {id: 4, name: 'kolya', age: 29, isMarried: true, address: {city: 'Lviv', street: 'Pasichna', number: 121}},
+//     {id: 3, name: 'olya', age: 28, isMarried: false, address: {city: 'Rivne', street: 'Shevchenko', number: 90}},
+//     {id: 8, name: 'max', age: 30, isMarried: true, address: {city: 'Lviv', street: 'Kriva Lipa', number: 115}},
+//     {id: 6, name: 'anya', age: 31, isMarried: false, address: {city: 'Lviv', street: 'Shevchenko', number: 2}},
+//     {id: 10, name: 'oleg', age: 28, isMarried: false, address: {city: 'Kyiv', street: 'Centralna', number: 22}},
+//     {id: 5, name: 'andrey', age: 29, isMarried: true, address: {city: 'Lviv', street: 'Gorodotska', number: 43}},
+//     {id: 1, name: 'masha', age: 30, isMarried: true, address: {city: 'Kyiv', street: 'Peremogi', number: 12}},
+//     {id: 7, name: 'olya', age: 31, isMarried: false, address: {city: 'Lviv', street: 'Naukova', number: 16}},
+//     {id: 11, name: 'max', age: 31, isMarried: true, address: {city: 'Rivne', street: 'Ivana Franka', number: 121}}
+// ];
+//
+// const main = document.getElementById('main');
+// const render = (userArr, target) => {
+//     target.innerHTML = '';
+//     userArr.forEach(value => {
+//         const div = document.createElement('div');
+//         div.innerText = JSON.stringify(value);
+//         target.appendChild(div)
+//     })
+// }
+// render(usersWithAddress, main)
+//
+// const form = document.forms.myForm;
+// form.onchange = () => {
+//     const {status, gt, city} = form
+//     let res = usersWithAddress;
+//     // 1й - отфильтровывает пользователей со статусом false (осталяет со статусом false)
+//     if (status.checked) {
+//         res = res.filter(value => !value.status);
+//     }
+//     // 2й - оставляет старше 29 лет включительно
+//     if (gt.checked) {
+//         res = res.filter(value => value.age >= 29)
+//     }
+//     // 3й - оставляет тех у кого город киев
+//     if (city.checked) {
+//         res = res.filter(value => value.address.city === 'Kyiv')
+//     }
+//     render(res, main);
+// }
