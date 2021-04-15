c1ccb76ca86037ae11db8d8884f69ee1be520c7b
Andrii Hrytsai <andriyhrytsay@gmail.com>
2021-04-15 19:19:59 +0300

    hm_8

---
+++ hm_8/main.js
@@ -0,0 +1,97 @@
+//- Дана textarea.
+// В неё вводится текст.
+// Сделайте так, чтобы после захода на эту страницу через некоторое время, введенный текст остался в textarea.
+//
+// let areaText = document.getElementById('area');
+//  areaText.oninput = () =>{
+//      localStorage.setItem('textArea', areaText.value);
+//  }
+// let text = localStorage.getItem('textArea');
+// areaText.innerHTML = text;
+
+//- Дана форма с инпутами, текстареа, чекбоксами, радио кнопочками, селектами и тп.
+// Пользователь вводит какие-то данные и закрывает страницу (не факт, что он заполнил всю форму).
+// Сделайте так, чтобы при следующем заходе на страницу введенные им ранее данные стояли на своих местах.
+// Сделайте ваш скрипт как можно более универсальным.
+//
+//******** Інтпут ****************************
+// let input = document.getElementById('input');
+// input.oninput = () => {
+//     localStorage.setItem('inputTextArea', input.value);
+// }
+// let text = localStorage.getItem('inputTextArea');
+// input.value = text;
+//
+//**** Чекбокс ****************************
+//
+// let checker = document.getElementById('checker');
+// checker.onclick = () => {
+//    localStorage.setItem('checkOrNo',checker.checked)
+// }
+// let checkResult = localStorage.getItem('checkOrNo');
+// checker.checked = JSON.parse(checkResult);
+//
+//***Radio ****************************
+//
+// let radiOne = document.getElementById('radioOne');
+// radiOne.onclick = () => {
+//     localStorage.setItem('checkOrNoo',radiOne.checked)
+// }
+// let radioResult = localStorage.getItem('checkOrNoo');
+// radiOne.checked = JSON.parse(radioResult );
+//
+// TextArea ****************************
+//
+// let areaTextTwo = document.getElementById('areaTwo');
+//  areaTextTwo.oninput = () =>{
+//      localStorage.setItem('textAreaTwo', areaTextTwo.value);
+//  }
+// let textTwo = localStorage.getItem('textAreaTwo');
+// areaTextTwo.innerHTML = textTwo;
+//
+// Select ****************************
+//
+// let selectOne = document.getElementById('selectOne');
+// selectOne.oninput = () =>{
+//     localStorage.setItem('textSelector', selectOne.value);
+// }
+// let textSelector = localStorage.getItem('textSelector');
+// selectOne.value = textSelector;
+
+//-Дан текстареа. В него можно ввести данные, нажать кнопку "сохранить" и они "фикисруются" (в хранилище),
+// затем поредактировать их, затем еще поредактировать и возможно еще.....
+// Требование : хранить историю своих изменений (даже после перезагрузки страницы).
+// Сверху над текстареа должны появится стрелочки, с помощью которых можно перемещаться по
+// истории (не забудьте!чекпоинт истории - нажатеи кнопки сохранить).
+
+let areaThree = document.getElementById('areaThree');
+let continueButton = document.getElementById('continue');
+let arr = [];
+
+continueButton.onclick = () => {
+    arr.push(areaThree.value);
+    localStorage.setItem('text', JSON.stringify(arr));
+}
+let parserText = JSON.parse(localStorage.getItem('text'));
+// for (let parserTextElement of parserText) {
+//     console.log(parserTextElement);
+// }
+let minus = document.getElementById('minus');
+let a = 1;
+minus.onclick = () => {
+    areaThree.value = parserText[parserText.length - a];
+    if (a === parserText.length) {
+        return 0;
+    }
+    a++
+}
+let plus = document.getElementById('plus');
+let y = parserText.length - a;
+plus.onclick = () => {
+    areaThree.value = parserText[y - 1]
+    if (y === parserText.length) {
+        return 0;
+    }
+    y++;
+}
+
